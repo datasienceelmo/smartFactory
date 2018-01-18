700fc6d929d25c65fce1697d468356f45f356efc
E.Lmo <32750151+datasienceelmo@users.noreply.github.com>
2018-01-18 12:59:23 +0100

    Add files via upload

--- maschine1/main.py
+++ maschine1/main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+import write_blue
+import write_red
+import write_purple
+import write_yellow
 
 #Dauerschleife zum permanenten Beschreiben der Transponder
 while True:
@@ -10,22 +14,23 @@ while True:
     #Ueberpruefung der Eingabe und Aufruf der entsprechenden Schreibdatei (write_Farbe)
     if terminal_input == "blau":
         print "Produkt blau: Bitte Transponder zum Beschreiben auf das Lesegerät halten"
-        import write_blue
+        # Aufruf der richtigen Schreibdatei mittels .init(), da bei Aufruf per Import-Funktion diese sofort ausgefuehrt werden wuerde
+        write_blue.init() 
         print "Schreibvorgang abgeschlossen"
 
     elif terminal_input == "rot":
         print "Produkt rot: Bitte Transponder zum Beschreiben auf das Lesegerät halten"
-        import write_red
+        write_red.init()
         print "Schreibvorgang abgeschlossen"
         
     elif terminal_input == "lila":
         print "Produkt lila: Bitte Transponder zum Beschreiben auf das Lesegerät halten"
-        import write_purple
+        write_purple.init()
         print "Schreibvorgang abgeschlossen"
         
     elif terminal_input == "gelb":
         print "Produkt gelb: Bitte Transponder zum Beschreiben auf das Lesegerät halten"
-        import write_yellow
+        write_yellow.init()
         print "Schreibvorgang abgeschlossen"
 
     #Ausgabe bei Falscheingabe
